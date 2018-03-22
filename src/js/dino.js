3af050152b71bb28427f056d0e037c73a9059935
Emily Watkins and Sara Hamilton <sara@>
2018-03-22 16:04:14 -0700

    add line chart image of last two letters

--- src/js/dino.js
+++ src/js/dino.js
@@ -11,6 +11,7 @@ export class Dino {
     $.get(`http://dinoipsum.herokuapp.com/api/?format=json&paragraphs=${this.paragraph_count}&words=${this.word_count}`)
     .then((response) => {
       this.countFirstLetters(response);
+      this.lineChart(response);
       displayData(response);
     })
     .fail(function(error) {
@@ -39,8 +40,35 @@ export class Dino {
     let keysString = keys.join(",");
     let values = Array.from(arrayMap.values());
     let valuesString = values.join(",");
-    let url =    `https://api.labstack.com/chart/bar?values=${valuesString}&labels=${keysString}&titles=First_Letter&colors=lightseagreen&title=Dino%20Stats&x_title=First_Letter`;
+    let url =    `https://api.labstack.com/chart/bar?values=${valuesString}&labels=${keysString}&colors=lightseagreen&title=Begins%20With&x_title=First%20Letter`;
     $('#chart').html(`<img src=${url}>`);
+  }
+
+  lineChart(array) {
+    const lineMap = new Map;
+    for(let i = 0; i < array.length; i++) {
+      for (let j = 0; j < array[i].length; j++) {
+        let lastTwo = array[i][j].slice(-2);
+        if (lineMap.has(lastTwo) === true) {
+          let currentValue = lineMap.get(lastTwo);
+          lineMap.set(lastTwo, currentValue+=1);
+        } else {
+          lineMap.set(lastTwo, 1);
+        }
+      }
+    }
+    this.getLineChart(lineMap);
+  }
+
+  getLineChart(lineMap) {
+    let lineKeys = Array.from(lineMap.keys());
+    let lineKeysString = lineKeys.join(",");
+    console.log("keysString", lineKeysString);
+    let lineValues = Array.from(lineMap.values());
+    let lineValuesString = lineValues.join(",");
+    let lineurl =
+    `https://api.labstack.com/chart/line?values=${lineValuesString}&labels=${lineKeysString}&title=Ends%20With&x_title=Last%20Two%20Letters`;
+    $('#linechart').html(`<img src=${lineurl}>`);
   }
 
 }
